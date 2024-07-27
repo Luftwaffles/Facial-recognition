fae47eb0e7fc2accdb51aefcb4004bea05e6bba9
Luftwaffles <sunnyhoang314@gmail.com>
2024-07-27 02:18:27 -0600

    removed virtual environment

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,14 +1,30 @@
 import cv2
-# import face_recognition
+import face_recognition
 # import pickle
 import os
 
 # Importing the student images
 folderPath = 'Images'
-modePathList = os.listdir(folderPath)
+pathList = os.listdir(folderPath)
+print(pathList)
 imgList = []
-for path in modePathList:
+studentIds = []
+for path in pathList:
         imgList.append(cv2.imread(os.path.join(folderPath,path)))
-print(len(imgList))
+        studentIds.append(os.path.splitext(path)[0])
 
-# print(len(imgModeList))
+print(studentIds)
+
+
+def findEncodings(imagesList):
+        encodeList = []
+        for img in imagesList:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                encode = face_recognition.face_encodings(img)[0]
+                encodeList.append(encode)
+
+        return encodeList
+print("Encoding Started...")
+encodeListKnown = findEncodings(imgList)
+print(encodeListKnown)
+print("Encoding Complete")
