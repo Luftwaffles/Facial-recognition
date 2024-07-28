bfa389b33af1fcab3915eb50de244f1b241b867b
Luftwaffles <sunnyhoang314@gmail.com>
2024-07-28 04:17:22 -0600

    face recognition module complete

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import cv2
 import pickle
+import cvzone
+import numpy as np
+import face_recognition
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 640)
@@ -16,18 +19,42 @@ for path in modePathList:
         imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
 
 # Load the encoding file
+print("Loading Encode file...")
 file = open('EncodeFile.p', 'rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
 encodeListKnown, studentIds = encodeListKnownWithIds
+print("Encode File loaded...")
 
 while True:
         success, img = cap.read()
 
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        faceCurrFrame = face_recognition.face_locations(imgS)
+        encodeCurrFrame = face_recognition.face_encodings(imgS, faceCurrFrame)
+
+
         imgBackground[162:162 + 480, 55:55 + 640] = img
         imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[1]
 
-        # cv2.imshow("Webcam", img)
+        for encodeFace, faceLoc in zip(encodeCurrFrame, faceCurrFrame):
+                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                
+                matchIndex = np.argmin(faceDis)
+
+                if matches[matchIndex]:
+                        # print("Known Face Detected")
+                        # print(studentIds[matchIndex])
+                        y1, x2, y2, x1 = faceLoc
+                        y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                        bbox = 55+x1, 162+y1, x2-x1, y2-y1
+                        imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+
+
+        cv2.imshow("Webcam", img)
         cv2.imshow("Face Attendance", imgBackground)
         cv2.waitKey(1)        
         
