e6330403ccebab14ed4fff798d7486999e5a0911
Luftwaffles <sunnyhoang314@gmail.com>
2024-07-30 04:12:37 -0600

    add images to database

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-recognition-3e5be-default-rtdb.firebaseio.com/",
+    'storageBucket': "face-recognition-3e5be.appspot.com"
+})
 
 # Importing the student images
 folderPath = 'Images'
@@ -12,6 +22,11 @@ studentIds = []
 for path in pathList:
         imgList.append(cv2.imread(os.path.join(folderPath,path)))
         studentIds.append(os.path.splitext(path)[0])
+
+        fileName = f'{folderPath}/{path}'
+        bucket = storage.bucket()
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
 
 print(studentIds)
 
@@ -24,6 +39,7 @@ def findEncodings(imagesList):
                 encodeList.append(encode)
 
         return encodeList
+
 print("Encoding Started...")
 encodeListKnown = findEncodings(imgList)
 encodeListKnownWithIds = [encodeListKnown, studentIds]
