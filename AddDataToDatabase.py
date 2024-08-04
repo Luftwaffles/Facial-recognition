4c3711e037861453a12e825f7d99e6f49c2ad9c3
Luftwaffles <sunnyhoang314@gmail.com>
2024-08-03 23:33:33 -0600

    Update Attendance complete

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -40,21 +40,23 @@ data = {
             "year": 4,
             "last_attendance_time": "2024-7-29 13:32:45"
         },
-    "100001":
+    "100002":
         {
             "name": "Elon Musk",
             "major": "engineering",
             "starting_year": 2022,
             "total_attendance": 69,
+            "standing": "good",
             "year": 6,
             "last_attendance_time": "2024-7-29 13:32:45"
         },
-    "100002":
+    "100001":
         {
             "name": "Emily Blunt",
             "major": "fashion",
             "starting_year": 2015,
             "total_attendance": 42,
+            "standing": "good",
             "year": 3,
             "last_attendance_time": "2024-7-29 13:32:45"
         },
